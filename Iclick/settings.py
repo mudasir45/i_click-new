01f1213a5c2e82f8bf5a69b56af4832ad150f0af
Mudassar Amin <mudasiramin456@gmail.com>
2023-02-21 09:38:20 +0500

    commit

--- Iclick/settings.py
+++ Iclick/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e9pt^nglw+!xhw)n-sx4s%3ap^k+^p1a9uo@)8g78o%zn$yx6r
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'iclickevent.herokuapp.com']
 
 # DEBUG = False
 # ALLOWED_HOSTS = ['*']
