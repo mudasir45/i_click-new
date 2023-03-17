0d5c796173c482f0330eaf83a2ddc26de61f11fd
Mudassar Amin <mudasiramin456@gmail.com>
2023-03-17 10:28:29 +0500

    commite

--- Iclick/settings.py
+++ Iclick/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , "static"),
